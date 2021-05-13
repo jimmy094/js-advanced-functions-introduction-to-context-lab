226ba1cbd0e7937bcb55a8c1b5a8307a44292a1e
jimmyalvarez <jimmyalvarezjr@gmail.com>
2021-05-12 20:15:05 -0400

    commit tree 114d5e8806dcddcfe86c3cdf287964e1b7fa4ce6

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Your code here
+ let createEmployeeRecord = function(inputsArr){
+     return {
+     firstName: inputsArr[0],
+     familyName: inputsArr[1],
+     title: inputsArr[2],
+     payPerHour: inputsArr[3],
+     timeInEvents: [],
+     timeOutEvents: []
+        
+    }
+}    
+
+
+createEmployeeRecord([Jimmy, Alvarez, manager, fiftythousand])
